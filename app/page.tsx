05aabf5711a7db22d9cb3491c4e8ba59d9b0eb7d
miku-wwl <569144003@qq.com>
2024-04-24 02:09:22 +0800

    feat:pnpm & Home Page

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,41 @@
+import { buttonVariants } from "@/components/ui/button";
+import { siteConfig } from "@/config/site";
+import { cn, sortPosts } from "@/lib/utils";
+import Link from "next/link";
+import { posts } from "#site/content";
+import { PostItem } from "@/components/post-items";
+
 export default function Home() {
-  return <section>hello world</section>;
+  const latestPosts = sortPosts(posts).slice(0, 5);
+  return (
+    <>
+      <section className="space-y-4 pb-6 pt-6 md:pb-12 md:mt-10 ">
+        <div className="container flex flex-col gap-4 text-center">
+          <h1 className="text-3xl sm:text-5xl md:text-6xl lg:text-7xl font-black text-balance">
+            Weilai's Blog
+          </h1>
+          <p className="max-w-[42rem] mx-auto text-muted-foreground sm:text-xl text-balance">
+            Built by tailwind, shadcn, velit and Next.js 14.
+          </p>
+        </div>
+      </section>
+      <section className="container max-w-4xl py-6 lg:py-10 flex flex-col space-y-6">
+        <h2 className="text-3xl sm:text-5xl md:text-6xl lg:text-7xl font-black text-center">
+          Latest Posts
+        </h2>
+        <ul className="flex flex-col">
+          {latestPosts.map((post) => (
+            <li key={post.slug} className="first:border-t first:border-border">
+              <PostItem
+                slug={post.slug}
+                title={post.title}
+                description={post.description}
+                date={post.date}
+              />
+            </li>
+          ))}
+        </ul>
+      </section>
+    </>
+  );
 }
